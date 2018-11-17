ff77a7c7edf6ddb3102c0a6a8c8d37bc8fc01b2d
Huy Ngo <huybngo@yahoo.com>
2018-11-17 16:27:25 -0600

    Store probabilities in 2 tables Naive Bayes
    * Naive Bayes now stores 2 separate tables (DataFrame object) containing conditional probabilities for categorical and continuous features

--- predictors/naiveBayes.py
+++ predictors/naiveBayes.py
@@ -6,19 +6,22 @@ from model import SupervisedModel
 
 class NaiveBayes(SupervisedModel):
 	def __init__(self, targetFeature):
+		''' '''
 		super().__init__(targetFeature)
+		self._categoricalProbTable = None
+		self._continuousProbTable = None
 
 	def train(self, dataFrame):
-		priorProbabilities = self._getPriorProbabilities(dataFrame)
+		''' '''
 		return self._getConditionalProbabilities(dataFrame)
-		# print(priorProbabilities)
+		# print(labelProbabilities)
 		# print(super()._getFeatureType(dataFrame, "Group"))
 
 	# def classify(self, dataFrame):
 	# 	''' '''
 	# 	pass
 
-	def _getPriorProbabilities(self, dataFrame):
+	def _getLabelProbabilities(self, dataFrame):
 		''' '''
 		probabilities = {}
 		labelCounts = dataFrame[self._targetFeature].value_counts()
@@ -29,61 +32,44 @@ class NaiveBayes(SupervisedModel):
 	def _getConditionalProbabilities(self, dataFrame):
 		''' '''
 		features = dataFrame.loc[:, dataFrame.columns != self._targetFeature].columns.values
-		priorProbabilities = self._getPriorProbabilities(dataFrame)
+		labelProbabilities = self._getLabelProbabilities(dataFrame)
 		labelCounts = dataFrame[self._targetFeature].value_counts()
-		table = pd.DataFrame(index=labelCounts.index)
-
-		# for feature in features:
-		# 	featureType = super()._getFeatureType(dataFrame, feature)
-		# 	if (featureType == "categorical"):
-		# 		conditionalFeatureProbs = {} #{"Legendary": 0.98, "Ultra": 0.02}}
-
-		# 		for label in labelCounts.index:
-		# 			productProb = 1
-		# 			labelDataFrame = dataFrame[dataFrame[self._targetFeature] == label]
-		# 			valueCounts = labelDataFrame[feature].value_counts()
-		# 			for value in valueCounts.index:
-		# 				conditionalFeatureProbs[(feature, value)] = valueCounts[value] / len(labelDataFrame)
-		# 			conditionalFeatureProbs[label] = productProb
-
-
-		# 		table[feature] = conditionalFeatureProbs
-		# 		# print(conditionalFeatureProbs)
-		# 		break
+		#table = pd.DataFrame(index=labelCounts.index)
+		self._categoricalProbTable = pd.DataFrame(index=labelCounts.index)
+		self._continuousProbTable = pd.DataFrame(index=labelCounts.index)
 
 		# Need to handle zero probability
-		# Need to handle zero variance
+		# Need to handle zero standard deviation
 		# Need to see all possible feature values of each feature
 		for label in labelCounts.index:
 			labelDataFrame = dataFrame[dataFrame[self._targetFeature] == label]
 			for feature in features:
 				featureType = super()._getFeatureType(dataFrame, feature)
-				if (featureType == "categorical"): pass
-					# valueCounts = labelDataFrame[feature].value_counts()
-					# for value in valueCounts.index:
-					# 	probability = valueCounts[value] / len(labelDataFrame)
-					# 	columnName = "{0}={1}".format(feature, value)
-					# 	table.loc[label, columnName] = probability
+				if (featureType == "categorical"):
+					valueCounts = labelDataFrame[feature].value_counts()
+					for value in valueCounts.index:
+						probability = valueCounts[value] / len(labelDataFrame)
+						columnName = "{0}={1}".format(feature, value)
+						self._categoricalProbTable.loc[label, columnName] = probability
 				elif (featureType == "continuous"):
-					# raise NotImplementedError()
 					mean = labelDataFrame[feature].mean()
 					std = labelDataFrame[feature].std()
-					#print(label, feature, mean, std, 2 * (std**2) * math.pi)
 					
 					# Skip zero standard deviation feature
-					if (std == 0.0):
-						continue
+					if (std != 0.0):
+						#print("Saving mean & std of feature {0} -- label {1} -- mean {2} -- std {3}".format(feature, label, mean, std))
+						columnMean = "{0}=mean".format(feature)
+						columnStd = "{0}=std".format(feature)
+						self._continuousProbTable.loc[label, columnMean] = mean
+						self._continuousProbTable.loc[label, columnStd] = std
+					else: print("****Skipping {0} feature for label {1}. Standard deviation = 0".format(feature, label))
+		
 
-					operand1 = (1 / (math.sqrt(2 * (std**2) * math.pi)))
-					for value in labelDataFrame[feature]:
 
-						operand2 = math.exp(-(value - mean)**2 / (2 * std**2))
-						probability = operand1 * operand2
-
-						columnName = "{0}={1}".format(feature, value)
-						table.loc[label, columnName] = probability
-
-		return table.fillna(0.0)
+	@staticmethod
+	def ComputeGaussianProbability(value, mean, std):
+		''' '''
+		return (1 / (math.sqrt(2 * (std**2) * math.pi))) * math.exp(-(value - mean)**2 / (2 * std**2))
 				
 
 		
